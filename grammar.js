04d9bc42a79cca86ad1a0571fd6b1082b0c11617
lukasjoc <45790416+lukasjoc@users.noreply.github.com>
2023-02-12 17:43:43 +0100

    decide to parse body as individual blocks for simplicity

--- grammar.js
+++ grammar.js
@@ -1,23 +1,34 @@
 module.exports = grammar({
     name: "todoreadme",
-
     word: $ => $.identifier,
 
     rules: {
         source_file: $ => repeat1($.block),
-        block: $ => seq($.header, $.text),
+        block: $ => seq(
+            $.start_of_line,
+            // the rest is just basic text
+            $.text,
+        ),
 
-        header: $ => choice($.standard_header, $.category_header),
+        // a header variant with categories
+        // a list or some text that needed wrapping
+        start_of_line: $ => choice(
+            $.standard_header,
+            $.category_header,
+            $.li,
+            $.wrapped_text,
+        ),
+
         standard_header: $ => seq($.identifier, ":"),
         category_header: $ => seq($.identifier, $.categories, ":"),
-
         categories: $ => seq("[", repeat1($.category), "]"),
         category: $ => seq($.category_identifier, optional(",")),
 
         identifier: _$ => /(current|task|idea|later)/,
-        category_identifier: _$ => /[a-zA-Z0-9-_:@#]+/,
-
-        // TODO: support lists
+        category_identifier: _$ => /[a-zA-Z0-9-_]+/,
+        li: $ => seq(/- /, $.text),
+        wrapped_text: $ => seq("| ", $.text),
         text: _$ => /.*/,
     },
 });
+
